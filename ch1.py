6228bdf11b99ac9fa0e5aaa5b72c47a25253d728
qingminliu <953621303@qq.com>
2019-09-11 16:33:15 +0800

    update 9.11

--- ch1.py
+++ ch1.py
@@ -50,6 +50,52 @@ def npXOR(x1, x2):
     return tmp
 
 
-print(npXOR(1, 0))
-print(npXOR(1, 1))
-print(npXOR(0, 0))
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def identify_function(x):
+    return x
+
+
+def init_network():
+    network = dict()
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['B1'] = np.array([0.1, 0.2, 0.3])
+    network['B2'] = np.array([0.1, 0.2])
+    network['B3'] = np.array([0.1, 0.2])
+
+    return network
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['B1'], network['B2'], network['B3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identify_function(a3)
+
+    return y
+
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+a = 1
+print(y)
+
